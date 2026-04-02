bcbebbca10c1ea7b9d31e55562b41fe0cc4f716f
Omed Mehdizada <omed.mehdi@gmail.com>
2026-04-02 20:06:49 +0200

    overengineerd af

---
+++ next-intl.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next-intl').NextIntlConfig} */
+const nextIntlConfig = {
+  locales: ['de', 'en'],
+  defaultLocale: 'de',
+  localeDetection: false,
+};
+
+export default nextIntlConfig;
